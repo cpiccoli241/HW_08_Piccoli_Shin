670ef34d0e6f9c79b2371252fa48141adda60ba3
Chris Piccoli <cap9763@rit.edu>
2020-10-24 13:44:46 -0400

    euc_distance of data_points

--- agglomerative_cluster.py
+++ agglomerative_cluster.py
@@ -1,19 +1,40 @@
+
+import numpy as np
+
 class data_point:
-    def __init__(self, x_loc, y_loc):
-        self.x_loc = x_loc
-        self.y_loc = y_loc
+    def __init__(self, vector_location, size=20):
+        '''
+
+        :param vector_location:type np.narray
+        :param size:
+        '''
+        if len(vector_location) != size:
+            self = None
+        else:
+            self.location = vector_location
+
+def euc_distance(data_point_1, data_point_2):
+    return np.linalg.norm(data_point_1.location-data_point_2.location)
 
 class cluster:
     '''
     number datapoints
     cluster center x, y
-    np.array datapoints
+    list datapoints
     '''
     def __init__(self, cluster1, cluster2):
         if isinstance(cluster1, data_point):
             if isinstance(cluster2, data_point):
                 self.center = cluster1
+                self.data_points = [cluster1, cluster2]
+            else:
+                self.data_points = [cluster1].extend(cluster2.data_points)
+        else:
+            self.data_points = cluster1.data_points.extend(cluster2.data_points)
+
+        self.left_cluster = cluster1
+        self.right_cluster = cluster2
 
 
-    def newCenter(self, otherCluster):
+    def new_center(self, otherCluster):
         return;
