dbc96653d98990dae74464b441fb16bf579dde0a
Chris Piccoli <cap9763@rit.edu>
2020-10-24 13:25:52 -0400

    added a file and stubbed it out

---
+++ agglomerative_cluster.py
@@ -0,0 +1,18 @@
+class data_point:
+    def __init__(self, x_loc, y_loc):
+        self.x_loc = x_loc
+        self.y_loc = y_loc
+
+class cluster:
+    '''
+    number datapoints
+    cluster center x, y
+    np.array datapoints
+    '''
+    def __init__(self, cluster1, cluster2):
+        if isinstance(cluster1, data_point):
+
+        if isinstance(cluster2, data_point):
+
+
+    def newCenter(self, otherCluster):
